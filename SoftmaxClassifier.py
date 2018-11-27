1c26403951015eda9b664662cddee94c64439585
ManelBoucenna <manal-maroua.boucenna@g.enp.edu.dz>
2018-11-27 13:59:57 -0500

    MiSE A JOUR

--- SoftmaxClassifier.py
+++ SoftmaxClassifier.py
@@ -1,23 +1,12 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
-import copy
+import math as mt
 
 class SoftmaxClassifier(BaseEstimator, ClassifierMixin):  
     """A softmax classifier"""
 
     def __init__(self, lr = 0.1, alpha = 100, n_epochs = 1000, eps = 1.0e-5,threshold = 1.0e-10 , regularization = True, early_stopping = True):
        
-        """
-            self.lr : the learning rate for weights update during gradient descent
-            self.alpha: the regularization coefficient 
-            self.n_epochs: the number of iterations
-            self.eps: the threshold to keep probabilities in range [self.eps;1.-self.eps]
-            self.regularization: Enables the regularization, help to prevent overfitting
-            self.threshold: Used for early stopping, if the difference between losses during 
-                            two consecutive epochs is lower than self.threshold, then we stop the algorithm
-            self.early_stopping: enables early stopping to prevent overfitting
-        """
-
         self.lr = lr 
         self.alpha = alpha
         self.n_epochs = n_epochs
@@ -28,39 +17,6 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
         
 
 
-    """
-        Public methods, can be called by the user
-        To create a custom estimator in sklearn, we need to define the following methods:
-        * fit
-        * predict
-        * predict_proba
-        * fit_predict        
-        * score
-    """
-
-
-    """
-        In:
-        X : the set of examples of shape nb_example * self.nb_features
-        y: the target classes of shape nb_example *  1
-
-        Do:
-        Initialize model parameters: self.theta_
-        Create X_bias i.e. add a column of 1. to X , for the bias term
-        For each epoch
-            compute the probabilities
-            compute the loss
-            compute the gradient
-            update the weights
-            store the loss
-        Test for early stopping
-
-        Out:
-        self, in sklearn the fit method returns the object itself
-
-
-    """
-
     def fit(self, X, y=None):
         
         prev_loss = np.inf
@@ -68,71 +24,60 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
 
         self.nb_feature = X.shape[1]
         self.nb_classes = len(np.unique(y))
-
         
-        X0 = np.ones((X.shape[0],1))
-        X_bias =  np.hstack((X0,X))      
-        self.theta_ = np.random.randint(5, size=(self.nb_feature + 1,self.nb_classes))
+
+        X_bias = np.c_[ np.ones(X.shape[0]), X ]        
+        self.theta_ = np.random.rand(self.nb_feature + 1, self.nb_classes)
         
 
         for epoch in range( self.n_epochs):
-
-            # logits = 
-            # probabilities = 
-            
-            
-            # loss =                
-            # self.theta_ = 
-            
-            # self.losses_.append(loss)
-
-            if self.early_stopping:
-                pass
-
-
+            #Je calcule les logits
+#             print('Old theta')
+#             print(self.theta_)
+            logits = X_bias @ self.theta_  
+            #Je transforme en proba avec softmax
+            p = self.predict_proba(X,y)
+#             print("Proba")
+#             print(p)
+            loss = self._cost_function(p, y) 
+#             print("Perte")
+#             print(loss)
+#             print("Gradient ")
+#             print(self._get_gradient(X_bias,y, p))
+            self.theta_ = self.theta_ - self.lr * self._get_gradient(X_bias,y, p)
+#             print(" newtheta_")
+#             print(self.theta_)
+            if (len(self.losses_) > 0 ):
+                diff = self.losses_[-1]-loss
+#                 print(diff)
+                self.losses_.append(loss)
+                if (abs(diff) < self.threshold):
+                    return self
+            else:
+                self.losses_.append(loss)
+                
         return self
 
     
-
-   
-    
-
-    """
-        In: 
-        X without bias
-
-        Do:
-        Add bias term to X
-        Compute the logits for X
-        Compute the probabilities using softmax
-
-        Out:
-        Predicted probabilities
-    """
-
     def predict_proba(self, X, y=None):
         try:
             getattr(self, "theta_")
-            
         except AttributeError:
             raise RuntimeError("You must train classifer before predicting data!")
-        
-        pass
-
-
-        """
-        In: 
-        X without bias
-
-        Do:
-        Add bias term to X
-        Compute the logits for X
-        Compute the probabilities using softmax
-        Predict the classes
-
-        Out:
-        Predicted classes
-    """
+#         print("X")
+#         print(X)
+#         print("Theta")
+#         print(self.theta_)
+        X_bias = np.c_[ np.ones(X.shape[0]), X ]
+#         print("X biais")
+#         print(X_bias)
+        z = np.dot(X_bias, self.theta_)
+#         print("Z")
+#         print(z)
+        p = self._softmax(z)
+#         print("Softmax")
+#         print(p)
+        return p
 
     
     def predict(self, X, y=None):
@@ -140,131 +85,69 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
             getattr(self, "theta_")
         except AttributeError:
             raise RuntimeError("You must train classifer before predicting data!")
-        pass
-
-    
+        temp = np.unique(y)
+        X_bias = np.c_[ np.ones(X.shape[0]), X ]
+        p = self.predict_proba(X, y)
+        predictions = p.argmax(axis=1)
+        return predictions #self._one_hot(predictions)
+        
 
     def fit_predict(self, X, y=None):
         self.fit(X, y)
-        return self.predict(X,y)
-
-
-    """
-        In : 
-        X set of examples (without bias term)
-        y the true labels
-
-        Do:
-            predict probabilities for X
-            Compute the log loss without the regularization term
-
-        Out:
-        log loss between prediction and true labels
-
-    """    
+        return self.predict(X,y)   
 
     def score(self, X, y=None):
-        pass
-    
+        probabilities = self.predict(X)
+        return self._cost_function(probabilities, y )
 
-    """
-        Private methods, their names begin with an underscore
-    """
-
-    """
-        In :
-        y without one hot encoding
-        probabilities computed with softmax
-
-        Do:
-        One-hot encode y
-        Ensure that probabilities are not equal to either 0. or 1. using self.eps
-        Compute log_loss
-        If self.regularization, compute l2 regularization term
-        Ensure that probabilities are not equal to either 0. or 1. using self.eps
-
-        Out:
-        Probabilities
-    """
-    
     def _cost_function(self,probabilities, y ): 
-        pass
-    
-
-    
-    """
-        In :
-        Target y: nb_examples * 1
-
-        Do:
-        One hot-encode y
-        [1,1,2,3,1] --> [[1,0,0],
-                         [1,0,0],
-                         [0,1,0],
-                         [0,0,1],
-                         [1,0,0]]
-        Out:
-        y one-hot encoded
-    """
-
-    
-    
-    def _one_hot(self,y): 
-        nb_example= len(y)
-        one_hot = np.zeros((nb_example, self.nb_classes))
-        i = 0
-        for x in y:
-            one_hot[i][x-1] = 1
-            i += 1
-        pass
-        return one_hot
-            
-
-    """
-        In :
-        Logits: (self.nb_features +1) * self.nb_classes
-
-        Do:
-        Compute softmax on logits
-
-        Out:
-        Probabilities
-    """
-    
-    
-    def _softmax(self,z):
-        sum_exp= copy.deepcopy(z)
-        lines = z.shape[0]
-        columns = z.shape[1]
-        _sum = 0
-        for x in range(columns):
-            for y in range(lines):
-                _sum += m.exp(z[y][x]) 
-            for i in range(lines):
-                sum_exp[i][x] = m.exp(z[i][x])/_sum
-            _sum = 0
-        #return np.exp(z)/np.sum(np.exp(z),axis=0)
-        pass
-    
-
-    """
-        In:
-        X with bias
-        y without one hot encoding
-        probabilities resulting of the softmax step
-
-        Do:
-        One-hot encode y
-        Compute gradients
-        If self.regularization add l2 regularization term
-
-        Out:
-        Gradient
-
-    """
-
-    def _get_gradient(self,X,y, probas):
+        m = probabilities.shape[0]
+        # One-hot encode y
+        y_oh = self._one_hot(y)
+        p = np.clip(probabilities, self.eps, 1-self.eps)
+        J = 0     
+        J = - np.sum(y_oh*np.log(p))/ m
+#         for i in range(y_oh.shape[0]):
+#             for k in range(y_oh.shape[1]):
+#                 J = J + y_oh[k][i]*mt.log(p[k][i])
+#         J= -J/m
+        if self.regularization:
+            l2 = 0
+#             for i in range(self.nb_feature):
+#                 for k in range(1,self.nb_classes):
+#                     l2 = l2 + self.theta_[k,i]**2
+#             l2 = l2 * self.alpha
+            l2= (self.alpha * np.sum(self.theta_[1:,:]**2))
+            J = J + l2
         
-        pass
+        return J
+
+    def _one_hot(self,y):
+        temp = np.unique(y)
+        oneHot = np.zeros((len(y), self.nb_classes))
+        j=0
+        for i in y:
+            index = list(temp).index(i)
+            oneHot[j][index]=1
+            j += 1
+        return oneHot
+
+    def _softmax(self,z):
+        SM = np.empty(z.shape)
+        temp = np.amax(z, axis = 1)
+        for i in range(z.shape[0]):
+            SM[i,:]  = np.exp(z[i,:] - temp[i])
+            SM[i,:] /= np.sum(SM[i,:])
+        return SM
+
+    def _get_gradient(self,X_bias,y, probas):
+        m = probas.shape[0]
+        loss = probas - self._one_hot(y)
+        xTrans = X_bias.transpose()
+        # Delta : number of features + 1 * number of classes 
+        delta = np.dot(xTrans, loss)
+        if self.regularization:
+            delta[1:,:] = delta[1:,:] + self.alpha * self.theta_[1:,:]
+        return delta/m
     
     
